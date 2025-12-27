1db7060d1851980154dc79b1a682ab39ef346656
Saneity <firdausarsad04@gmail.com>
2025-12-28 03:23:54 +0800

    testing eeprom save for calibration 	deleted:    compile_commands.json 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,45 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+#include <Preferences.h>
 #include <ESP32CYD_defines.h>
-
 TFT_eSPI tft = TFT_eSPI();
+Preferences preferences;
 uint16_t calData[5];
 int lastX = -1;
 int lastY = -1;
+
+bool loadCalibration();
 
 void setup(void) {
   // Initialise the TFT screen
   tft.init();
   tft.fillScreen(TFT_BLACK);
-  pinMode(LDR_PIN, INPUT);
-  // Set the rotation to the orientation you wish to use in your project before calibration
-  // (the touch coordinates returned then correspond to that rotation only)
   tft.setRotation(1);
-  tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
-  
+  // Save Data
+  if (!loadCalibration()) {
+    preferences.begin("Calibration", false);
+    tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
+
+    for (int i = 0; i < 5; i++) {
+      char key[6];
+      sprintf(key, "cal%d", i);
+      preferences.putUShort(key, calData[i]);
+    }
+    preferences.end();
+
+  }
+
   tft.setTouch(calData);
 
-  // Clear the screen
+    // Clear the screen
   tft.fillScreen(TFT_BLACK);
   tft.drawCentreString("Touch screen to test!",tft.width()/2, tft.height()/2, 2);
 }
 
 void loop() {
   uint16_t x = 0, y = 0; // To store the touch coordinates
-
+  
   bool pressed = tft.getTouch(&x, &y);
-
-  tft.drawString("LDR: " + String(ldrValue), 10, 10, 2);
-
    if (pressed) {
 
     if (lastX >= 0 && lastY >= 0) {
@@ -51,3 +60,21 @@ void loop() {
   delay(10);
 }
 
+bool loadCalibration() {
+  preferences.begin("Calibration", true);
+
+  for (int i = 0; i < 5; i++) {
+    char key[6];
+    sprintf(key, "cal%d", i);
+
+    if (!preferences.isKey(key)) {
+      preferences.end();
+      return false;   // calibration missing
+    }
+
+    calData[i] = preferences.getUShort(key);
+  }
+
+  preferences.end();
+  return true;        // calibration exists
+}
